88530656bf9884ddb2abe96ec1f40c658feb9f00
tmzy7 <tmzy7@163.com>
2016-11-13 17:33:51 +0800

    my first project exercise

---
+++ HelloWord/src/HelloWord.java
@@ -0,0 +1,12 @@
+
+public class HelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Word");
+	}
+
+}
